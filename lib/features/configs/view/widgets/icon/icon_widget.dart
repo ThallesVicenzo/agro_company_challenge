3c398b0a23b52e3c4d884cde7f84369cdbf22bb1
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-03-19 14:23:58 -0300

    feat: adds IconWidget

---
+++ lib/features/configs/view/widgets/icon/icon_widget.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../../../core/enums/icons_enum.dart';
+
+class IconWidget extends StatelessWidget {
+  final IconsEnum icon;
+  final Size size;
+  final BoxFit fit;
+  final Alignment alignment;
+  final Color? color;
+  final Color? backgroundColor;
+  final String? package;
+  final VoidCallback? onTap;
+  final BorderRadius? radius;
+  final String? customPath;
+  final Size? backgroundSize;
+  const IconWidget({
+    super.key,
+    required this.icon,
+    this.onTap,
+    this.size = const Size(24, 24),
+    this.fit = BoxFit.contain,
+    this.alignment = Alignment.center,
+    this.color = Colors.black,
+    this.package = 'design_system',
+    this.customPath,
+    this.radius,
+    this.backgroundColor,
+    this.backgroundSize,
+  });
+
+  String get path => customPath ?? icon.path;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      radius: 1,
+      splashColor: Colors.transparent,
+      child: ClipRRect(
+        borderRadius: radius ?? BorderRadius.circular(4),
+        child: Container(
+          decoration: BoxDecoration(
+            color: backgroundColor,
+          ),
+          constraints: BoxConstraints(
+            minHeight: size.height,
+            minWidth: size.width,
+            maxHeight: backgroundSize?.height ?? size.height,
+            maxWidth: backgroundSize?.width ?? size.width,
+          ),
+          child: Center(
+            child: SvgPicture.asset(
+              path,
+              semanticsLabel: icon.name,
+              width: size.width,
+              height: size.height,
+              fit: fit,
+              alignment: alignment,
+              theme: SvgTheme(
+                currentColor: color ?? Colors.transparent,
+              ),
+              package: package,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  factory IconWidget.byPath({
+    required String path,
+    Size size = const Size(24, 24),
+    BoxFit fit = BoxFit.contain,
+    Alignment alignment = Alignment.center,
+    Color? color,
+    VoidCallback? onTap,
+    Color? backgroundColor,
+    final Size? backgroundSize,
+  }) {
+    return IconWidget(
+      icon: IconsEnum.vector,
+      customPath: path,
+      size: size,
+      fit: fit,
+      alignment: alignment,
+      package: 'design_system',
+      color: color,
+      onTap: onTap,
+      backgroundColor: backgroundColor,
+      backgroundSize: backgroundSize,
+    );
+  }
+}
